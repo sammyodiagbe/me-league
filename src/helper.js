44cdaf94c816cc70a0e63fa6d8d294ceec7ea353
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-06-07 15:23:06 -0500

    working on application

---
+++ src/helper.js
@@ -0,0 +1,107 @@
+const teamsData = [
+  {
+    name: "Manchester United",
+    shortName: "MAN",
+    emblem: "https://resources.premierleague.com/premierleague/badges/t1.svg",
+  },
+  {
+    name: "Liverpool",
+    shortName: "LIV",
+    emblem: "https://resources.premierleague.com/premierleague/badges/t14.svg",
+  },
+  {
+    name: "Manchester City",
+    shortName: "MNC",
+    emblem: "https://resources.premierleague.com/premierleague/badges/t43.svg",
+  },
+  {
+    name: "Chelsea FC",
+    shortName: "CHE",
+    emblem: "https://resources.premierleague.com/premierleague/badges/t8.svg",
+  },
+  {
+    name: "Arsenal",
+    shortName: "ARS",
+    emblem: "https://resources.premierleague.com/premierleague/badges/t3.svg",
+  },
+  {
+    name: "AFC Bournemouth",
+    shortName: "BOU",
+    emblem: "https://resources.premierleague.com/premierleague/badges/t91.svg",
+  },
+  {
+    name: "Leeds United",
+    shortName: "LEE",
+    emblem: "https://resources.premierleague.com/premierleague/badges/t2.svg",
+  },
+  {
+    name: "Crystal Palace",
+    shortName: "CRY",
+    emblem: "https://resources.premierleague.com/premierleague/badges/t31.svg",
+  },
+  {
+    name: "Everton",
+    shortName: "EVE",
+    emblem: "https://resources.premierleague.com/premierleague/badges/t11.svg",
+  },
+  {
+    name: "Fulham",
+    shortName: "FUL",
+    emblem: "https://resources.premierleague.com/premierleague/badges/t54.svg",
+  },
+  {
+    name: "Nottingham Forest",
+    shortName: "NOT",
+    emblem: "https://resources.premierleague.com/premierleague/badges/t17.svg",
+  },
+  {
+    name: "Southhampton",
+    shortName: "SOU",
+    emblem: "https://resources.premierleague.com/premierleague/badges/t20.svg",
+  },
+  {
+    name: "Tottenham Hotspur",
+    shortName: "TOT",
+    emblem: "https://resources.premierleague.com/premierleague/badges/t6.svg",
+  },
+  {
+    name: "Liecester City",
+    shortName: "LIE",
+    emblem: "https://resources.premierleague.com/premierleague/badges/t13.svg",
+  },
+  {
+    name: "Westham United",
+    shortName: "WES",
+    emblem: "https://resources.premierleague.com/premierleague/badges/t21.svg",
+  },
+  {
+    name: "Wolvehampton Wanderers",
+    shortName: "WOV",
+    emblem: "https://resources.premierleague.com/premierleague/badges/t39.svg",
+  },
+  {
+    name: "Aston Villa",
+    shortName: "ASV",
+    emblem: "https://resources.premierleague.com/premierleague/badges/t7.svg",
+  },
+  {
+    name: "Brentford",
+    shortName: "BRE",
+    emblem: "https://resources.premierleague.com/premierleague/badges/t94.svg",
+  },
+  {
+    name: "Brighton",
+    shortName: "BRA",
+    emblem: "https://resources.premierleague.com/premierleague/badges/t36.svg",
+  },
+  {
+    name: "Newcastle United",
+    shortName: "NEW",
+    emblem: "https://resources.premierleague.com/premierleague/badges/t4.svg",
+  },
+];
+
+const weeks = 38;
+const totalGamesInSeason = 380;
+
+export default { teamsData, weeks, totalGamesInSeason };
---
+++ src/models/team.js
@@ -0,0 +1,23 @@
+
+
+class Team {
+    #name;
+    #emblem;
+    #shortName;
+    #points = 0;
+    #gamePlayed = 0;
+    #won = 0;
+    #drawn = 0;
+    #lost = 0;
+    #goalFor = 0;
+    #goalAgainst = 0;
+    #goalDone = 0;
+    #points = 0;
+    constructor(name, emblem, shortName) {
+        this.#name = name;
+        this.#emblem = emblem;
+        this.#shortName = shortName;
+    }
+}
+
+export default Team;
