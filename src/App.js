502fd94c3e483643f9d08e56b5484375d271fed6
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-06-20 16:07:07 -0500

    working on league simulation

--- src/App.js
+++ src/App.js
@@ -5,22 +5,11 @@ import Match from "./components/match";
 
 function App() {
   const gctx = useContext(gameContext);
-  const { generateLeagueData, fixtures, currentWeek, started } = gctx;
 
-  console.log(fixtures[0]);
-  const renderFixture = started ? (
-    fixtures[currentWeek].getGames.map((match, index) => {
-      return <Match key={index} match={match} />;
-    })
-  ) : (
-    <button className="trigger" onClick={generateLeagueData}>
-      Start League
-    </button>
-  );
   return (
     <div className="App">
       <div className="trigger-container" style={{}}>
-        {renderFixture}
+        <button className="start-simulation">Start Simulation</button>
       </div>
     </div>
   );
---
+++ src/components/game.js
@@ -0,0 +1,10 @@
+const Game = () => {
+  return (
+    <div className="game">
+      <div className="games"></div>
+      <div className="table"></div>
+    </div>
+  );
+};
+
+export default Game;
