26cabbe49bdac8f587dd17f6aa844789a626555c
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-06-29 09:40:27 -0500

    table and game screen

--- src/components/season_table.js
+++ src/components/season_table.js
@@ -1,18 +1,24 @@
 import { useContext } from "react";
 import { gameContext } from "../context/gameContext";
 import TeamEntry from "./team_table";
+import _ from "lodash";
 
 const SeasonTable = () => {
   const gtx = useContext(gameContext);
-  console.log(gtx);
   const { table } = gtx;
 
-  const renderTable = Array.from(table)
-    .sort((a, b) => a.getName && b.points - a.points)
+  const renderTable = _.toArray(table)
+    .sort((a, b) => a - b)
     .map((team, index) => {
       return <TeamEntry team={team} key={index} />;
     });
-  return <div className="season-table">{renderTable}</div>;
+
+  return (
+    <div className="season-table">
+      <h1>table</h1>
+      {renderTable}
+    </div>
+  );
 };
 
 export default SeasonTable;
