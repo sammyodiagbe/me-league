2985068aadead59fd19700ff1cdd189e74f4ce6f
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-06-29 11:53:03 -0500

    working on game

--- src/components/game.js
+++ src/components/game.js
@@ -4,23 +4,35 @@ import { useState } from "react";
 const Game = ({ match }) => {
   const { getTeam1, getTeam2, getTeam1Emblem, getTeam2Emblem } = match;
   const [gameTime, setGameTime] = useState(0);
-  const [scores, setScore] = useState([0, 0]);
+  const [homeScore, setHomeScore] = useState(0);
+  const [awayScore, setAwayScore] = useState(0);
 
   useEffect(() => {
-    const gameInt = setInterval(() => {
+    let gameInterval = setInterval(() => {
+      playGame();
       setGameTime((timer) => timer + 1);
     }, 3000);
 
-    return () => {
-      clearInterval(gameInt);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (gameTime > 0 && gameTime % 5 === 0) {
+    if (gameTime > 0 && gameTime % 4 === 0) {
       // stop the timer and update the team data on the table
+      console.log("game has ended");
+      clearInterval(gameInterval);
     }
+
+    return () => {
+      clearInterval(gameInterval);
+    };
   }, [gameTime]);
+
+  const playGame = () => {
+    const randomvalue = Math.floor(Math.random() * 2) + 1;
+
+    if (randomvalue === 1) {
+      setHomeScore((score) => score + 1);
+    } else {
+      setAwayScore((score) => score + 1);
+    }
+  };
 
   return (
     <div className="a-game">
@@ -30,9 +42,9 @@ const Game = ({ match }) => {
       <div className="score-holder">
         <img src={getTeam1Emblem} alt={getTeam1} />
         <div className="score-line">
-          <span className="score">{scores[0]}</span>
+          <span className="score">{homeScore}</span>
           <span className="versus">VS</span>
-          <span className="score">{scores[1]}</span>
+          <span className="score">{awayScore}</span>
         </div>
         <img src={getTeam2Emblem} alt={getTeam2} />
       </div>
